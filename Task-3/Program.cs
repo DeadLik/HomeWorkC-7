9af5f2df136622493dfcc518b722dc4dce4f1fcb
Игорь <mc_grove@bk.ru>
2022-12-02 18:42:22 -0500

    add task-3

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -7,3 +7,56 @@
 // 8 4 2 4
 
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ArithmeticMean(int[,] matrix)
+{
+    double arithmetic = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        arithmetic = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            arithmetic = (arithmetic + matrix[i, j]);
+        }
+        arithmetic /= matrix.GetLength(0);
+        Console.Write($"{Math.Round(arithmetic, 1)}; ");
+    }
+}
+
+// int[,] numbers = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+int[,] numbers = CreateMatrixRndInt(3, 4, 1, 10);
+ArithmeticMean(numbers);
+Console.WriteLine();
+Console.WriteLine();
+PrintMatrix(numbers);
+
